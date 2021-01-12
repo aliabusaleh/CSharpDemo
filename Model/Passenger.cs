5968cb0a9e46d76d4f670d274e6bab9e23654a3b
Ali AbuSaleh <ali.abusaleh.18@um.edu.mt>
2021-01-12 16:19:19 +0200

    Passenger Class

---
+++ Model/Passenger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiReservationProject.Model
+{
+    class Passenger : Person
+    {
+        public int Wallet { get; set; }
+
+        public Passenger(string child, string firstname = "", string lastname = "", string username = "", string password = "") : base(firstname, lastname, username, password)
+        {
+            Wallet = 0;
+        }
+        void addWalletMoney(int amount)
+        {
+            Wallet += amount;
+        }
+
+    }
+}
