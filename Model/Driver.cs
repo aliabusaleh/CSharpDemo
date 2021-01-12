bdf0c0493697eb2ee3e12e90e1e5bce30db94db6
Ali AbuSaleh <ali.abusaleh.18@um.edu.mt>
2021-01-12 16:18:52 +0200

    Driver Class and Fuctions including update for observer

---
+++ Model/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiReservationProject;
+
+namespace TaxiReservationProject.Model
+{
+    class Driver : Person
+    {
+       public int LicencsID  {set; get;}
+        public int Balance { set; get; }
+       public bool Avilabe { set; get; }
+
+      public  Driver(int licencsid, string firstname = "", string lastname = "", string username = "", string password = "", int balance = 0, bool avilable = true) : base(firstname, lastname, username, password)
+        {
+            LicencsID = licencsid;
+            Balance = balance;
+            Avilabe = avilable;
+
+        }
+      
+        public  void Update()
+        {
+            Console.WriteLine("Hello, I'm "+FirstName+" I've been notified from The Observer!!");
+        }
+    }
+  
+}
