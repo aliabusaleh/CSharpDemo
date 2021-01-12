5e124b384fcf305a1749871b125078df8420ff8a
Ali AbuSaleh <ali.abusaleh.18@um.edu.mt>
2021-01-12 16:19:52 +0200

    Parson class as parent for passenger and driver classes

---
+++ Model/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiReservationProject.Model
+{
+    class Person
+    {
+
+
+        public int UserID { set; get; }
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+
+       public Person(string firstname = "", string lastname = "" ,string username = "", string password = "")
+        {
+            UserID = generateId();
+            FirstName = firstname;
+            LastName = lastname;
+            Username = username;
+            Password = password;
+        }
+
+        static int generateId()
+        {
+            int id = 1;   //This resets the value to 1 every time you enter.
+            return id += 1;
+        }
+    }
+}
